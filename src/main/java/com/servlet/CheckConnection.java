20f91dfb47a3ed4f46866be80c201fcfe92bef47
Vinas Dharsandiya <vinasd3@gmail.com>
2018-06-16 23:31:39 +0530

    Connection check

---
+++ src/main/java/com/servlet/CheckConnection.java
@@ -0,0 +1,48 @@
+package com.servlet;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/CheckConnection")
+public class CheckConnection extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    public CheckConnection() {
+        super();
+    }
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		Connection con = null;
+		String result = null;
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			con = DriverManager.getConnection("jdbc:mysql://172.31.27.52:3306/test","user8E1","FO32gbK7b5TMSl6m");
+			if(con!=null)
+				result = "Connected !";
+		} catch (ClassNotFoundException | SQLException e) {
+				result = "Connection Error";
+				e.printStackTrace();
+		} finally {
+			if(con!=null)
+				try {
+					con.close();
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
+		}
+		response.getWriter().write(result);
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}
