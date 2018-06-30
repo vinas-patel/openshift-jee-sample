0db565c325f4abb4f3b178fa783035a8e5e8aaa6
Vinas Dharsandiya <40099176+vinas-patel@users.noreply.github.com>
2018-07-01 03:27:56 +0530

    check test
    just checking connection

--- src/main/java/com/servlet/CheckConnection.java
+++ src/main/java/com/servlet/CheckConnection.java
@@ -18,17 +18,18 @@ public class CheckConnection extends HttpServlet {
     public CheckConnection() {
         super();
     }
-
+	String host = "mysql";
+	String port = "3306";
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		Connection con = null;
 		String result = null;
 		try {
 			Class.forName("com.mysql.jdbc.Driver");
-			con = DriverManager.getConnection("jdbc:mysql://172.31.27.52:3306/test","user8E1","FO32gbK7b5TMSl6m");
+			con = DriverManager.getConnection("jdbc:mysql://"+host+":"+port+"/test","empcrud","empcrud");
 			if(con!=null)
 				result = "Connected !";
 		} catch (ClassNotFoundException | SQLException e) {
-				result = "Connection Error " + e.getMessage();
+				result = "Connection Error: " + e.getMessage();
 				e.printStackTrace();
 		} finally {
 			if(con!=null)
